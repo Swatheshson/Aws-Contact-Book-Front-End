f1c9097e8d73fc6e56ff6fee7b0ccb346f3fa86a
Swatheshson <swathesh.ags@gmail.com>
2025-12-03 19:01:38 +0530

    ng if, dom manipulation,just ngModel bindng

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('revis');
+
+   constructor(private http:HttpClient) {}
+   
+  //initial value
+  display:boolean = false;
+  //variable to hold values 
+  name:string = ""
+  numbervariable!:bigint
+  
+  change(){
+    this.display = true;
+  }
+  add(){
+    //should pass to the backend
+  }
 }
